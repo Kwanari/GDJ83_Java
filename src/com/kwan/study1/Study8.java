f662a1175e69b345eae6160f13b575ca8ff735ef
kwan <kwanda1227@gmail.com>
2024-05-27 17:30:45 +0900

    if,for ex1

--- src/com/kwan/study1/Study8.java
+++ src/com/kwan/study1/Study8.java
@@ -16,21 +16,25 @@ public class Study8 {
 		
 		Scanner sc = new Scanner(System.in);
 		
-		
 
 		for (int j=0;j<3;j++) {
+			System.out.println("");
 			System.out.println("1번 단발, 2번 점사");
+			
 			int bullet = sc.nextInt();
+			int count = 10;
+			String sound = "타타탕";
 			
 			if(bullet==1) {
-				for (int i=0;i<30;i++) {
-					System.out.print("탕");
-					}//i for 끝
-			} else {
-				for (int i=0;i<10;i++) {
-					System.out.print("타타탕");
-				}//i for 끝
-			}//else 끝
-		}//j for 끝
+					count=30;
+					sound="탕";
+			}
+			for (int i=0;i<count;i++) {
+				System.out.println(sound);
+			}
+		}//i for 끝
+		}
 	}
-}
+
+
+
