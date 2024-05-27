e7a39082806db9a56972b79fb98de05672ddf0a0
kwan <kwanda1227@gmail.com>
2024-05-27 16:56:29 +0900

    반복문 예제

--- src/com/kwan/study1/Study7.java
+++ src/com/kwan/study1/Study7.java
@@ -1,5 +1,7 @@
 package com.kwan.study1;
 
+import java.util.Scanner;
+
 public class Study7 {
 	public static void main (String [] args) {
 		//반복문 - for, while
@@ -7,11 +9,28 @@ public class Study7 {
 		//while (조건식)
 		//do{}while();
 		//break, continue
+		//3과목의 점수를 입력 받아 합계구하기
 		
-		for (int i=0;i<5;i++) {
-			System.out.println(i);
+		Scanner sc = new Scanner(System.in);
+		int sum = 0;
+		
+		for (int i=0;i<3;i++) {
+			int num = sc.nextInt();
+			//sum=num+sum;
+			sum+=num;
 		}
 		
+		//'a' - 'z' 출력
+		char c=97;
+		for (int i=0;i<26;i++) {
+			System.out.println(c);
+			c++;
+		}
+		for (int i='a';i<'z';i++) {
+			System.out.println((char)i);
+			c++;
+		}
+		System.out.println(sum);
 		System.out.print("abc");
 		System.out.println("def");
 		System.out.print("123");
---
+++ src/com/kwan/study1/Study8.java
@@ -0,0 +1,51 @@
+package com.kwan.study1;
+
+import java.util.Scanner;
+
+public class Study8 {
+	
+	
+	public static void main (String [] args) {
+		//이중 for
+		
+		//FPS
+		//총알 30발, 탄창 3개
+		
+		//1. 단발, 2. 점사
+		//탕 30번, 타타탕 10번
+		
+		Scanner sc = new Scanner(System.in);
+		
+		
+
+		for (int j=0;j<3;j++) {
+			System.out.println("1번 단발, 2번 점사");
+			int bullet = sc.nextInt();
+			
+			if(bullet==1) {
+				for (int i=0;i<30;i++) {
+					System.out.print("탕");
+					}
+			} else {
+				for (int i=0;i<10;i++) {
+					System.out.print("타타탕");
+				}
+			}
+		}
+
+		
+		
+		
+		
+		
+
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
