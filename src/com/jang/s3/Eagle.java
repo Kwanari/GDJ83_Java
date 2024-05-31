8d773caad4582df134f502626991f33e9f932bcf
jangdongguk <jangdong456@gmail.com>
2024-05-31 14:38:23 +0900

    5/31 study2

---
+++ src/com/jang/s3/Eagle.java
@@ -0,0 +1,18 @@
+package com.jang.s3;
+
+public class Eagle extends Bird implements Fly {
+	
+	@Override
+	public void flyAble() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public void sleep() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+}
