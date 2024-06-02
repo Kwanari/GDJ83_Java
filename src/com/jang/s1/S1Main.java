357fbaef331a79c8e08592226b9b08814b192dda
jangdongguk <jangdong456@gmail.com>
2024-06-02 20:20:53 +0900

    mac test

--- src/com/jang/s1/S1Main.java
+++ src/com/jang/s1/S1Main.java
@@ -1,15 +1,21 @@
 package com.jang.s1;
 
+import com.jang.s1.StackStudy;
+
 public class S1Main {
+	
 	public static void main(String[] args) {
 		Object obj = new Object();
 		
-		Test test = new Test();
-		String str = test.toString();
-		System.out.println(str);
+		int [] ar = {111,222,333};
 		
-		String name = new String("jang");
-		System.out.println(String.CASE_INSENSITIVE_ORDER);
-		System.out.println(name.toString());
+		Test test = new Test();
+//		String str = test.toString();
+//		System.out.println(str);
+		
+//		String name = new String("jang");
+//		System.out.println(String.CASE_INSENSITIVE_ORDER);
+//		System.out.println(name.toString());
+		
 	}
 }
