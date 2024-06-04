4aa27ee9dc7e842cba59e6456881f40a85e5ae11
kwan <kwanda1227@gmail.com>
2024-06-04 11:01:33 +0900

    wrapper예제2

--- src/com/kwan/s1/lang/wrapper/wrapper2.java
+++ src/com/kwan/s1/lang/wrapper/wrapper2.java
@@ -7,21 +7,23 @@ public class wrapper2 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
-		System.out.println("비밀번호 입력");
-		String pw = sc.next();
-
 		boolean flag = true;
 
-		for (int i = 0; i < pw.length(); i++) {
-			flag = Character.isUpperCase(pw.charAt(i));
-
-			if (flag) {
-				break;
+		while (flag) {
+			System.out.println("비밀번호 입력");
+			String pw = sc.next();
+			for (int i = 0; i < pw.length(); i++) {
+				flag = Character.isUpperCase(pw.charAt(i));
+				if (flag) {
+					flag = false;
+					break;
+				} else {
+					flag = true;
+					System.out.println("다시");
+					break;
+				}
 			}
-		}
 
-		if (!flag) {
-			System.out.println("다시");
 		}
 
 	}
