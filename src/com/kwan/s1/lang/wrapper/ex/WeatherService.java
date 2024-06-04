6377c760c27ccc487688ab8a5b4364ff8e5a53d0
Kwan <kwanda1227@gmail.com>
2024-06-04 21:08:14 +0900

    homework

--- src/com/kwan/s1/lang/wrapper/ex/WeatherService.java
+++ src/com/kwan/s1/lang/wrapper/ex/WeatherService.java
@@ -9,7 +9,7 @@ public class WeatherService {
 	public WeatherService() {
 		this.sb = new StringBuffer();
 		this.sb.append("서울 , 29.3 - 맑음 - 60");
-		this.sb.append("-부산 , 33.6 - 흐림 - 90");
+		this.sb.append("-부산 ,33.6 - 흐림 - 90");
 		this.sb.append("-제주 , 26.5 - 눈 - 30");
 		this.sb.append("-광주 , 10.6 - 태풍 - 80");
 	}
@@ -19,20 +19,30 @@ public class WeatherService {
 		String info = sb.toString(); // StringBuffer타입 참조변수를 String타입으로 변환
 
 		info = info.replace(",", "-");
+//		info = info.replace(" ", "");
 
 		String[] ar = info.split("-");
 
-		WeatherDTO wdto = new WeatherDTO();
+//		for (int i = 0; i < ar.length; i++) {
+//			System.out.println(ar[i]);
+//		}
 
-		for (int i = 0; i < ar.length; i++) {
-			for (int j = i * 4; j < 1; j++) {
-				wdto.setCity(ar[j].trim());
-				wdto.setGion(Double.parseDouble(ar[j + 1].trim()));
-				wdto.setStatus(ar[j + 2].trim());
-				wdto.setHumidity(Integer.parseInt(ar[j + 3].trim()));
+		WeatherDTO[] wdto = new WeatherDTO[ar.length / 4];
+
+		for (int i = 0; i < ar.length / 4; i++) {
+			for (int j = i * 4; j < j + 4; j++) {
+				wdto[i] = new WeatherDTO();
+				wdto[i].setCity(ar[j].trim());
+				wdto[i].setGion(Double.parseDouble(ar[j + 1].trim()));
+				wdto[i].setStatus(ar[j + 2].trim());
+				wdto[i].setHumidity(Integer.parseInt(ar[j + 3].trim()));
+				System.out.println(wdto[i].getCity());
+				System.out.println(wdto[i].getGion());
+				System.out.println(wdto[i].getStatus());
+				System.out.println(wdto[i].getHumidity());
+				break;
 			}
 		}
-
 	}
 
 }
