2b43672296a949391f7e87f2a91af18b44882eb0
kwan <kwanda1227@gmail.com>
2024-06-07 15:45:53 +0900

    list fin

--- src/com/kwan/s2/util/collections/ex/ProductMain.java
+++ src/com/kwan/s2/util/collections/ex/ProductMain.java
@@ -1,17 +1,34 @@
 package com.kwan.s2.util.collections.ex;
 
+import java.util.ArrayList;
+
 public class ProductMain {
 
 	public static void main(String[] args) {
 
-		ProductService ps = new ProductService();
+		// 데이터 핸들링
+		// 숫자형, 문자열, 논리형
 
-		for (int i = 0; i < 3; i++) {
-			System.out.println(ps.init().get(i).getName());
-			System.out.println(ps.init().get(i).getPrice());
-			System.out.println(ps.init().get(i).getStock());
+		ProductService ps = new ProductService();
+		ArrayList<ProductDTO> ar = ps.init();
+//		ps.addProduct(ar);
+		int result = ps.removeProduct(ar);
+
+		if (result > 0) {
+			System.out.println("성공");
+		} else {
+			System.out.println("실패");
 		}
 
+//		for (int i = 0; i < ar.size(); i++) {
+//			System.out.print("가격 ");
+//			System.out.println(ar.get(i).getPrice());
+//			System.out.print("제품명 ");
+//			System.out.println(ar.get(i).getName());
+//			System.out.print("재고 ");
+//			System.out.println(ar.get(i).getStock());
+//			System.out.println("=====================");
+//		}
 	}
 
 }
--- src/com/kwan/s2/util/collections/ex/ProductService.java
+++ src/com/kwan/s2/util/collections/ex/ProductService.java
@@ -1,16 +1,19 @@
 package com.kwan.s2.util.collections.ex;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class ProductService {
 	private StringBuffer sb;
+	private Scanner sc;
 
 	public ProductService() {
+		sc = new Scanner(System.in);
 		sb = new StringBuffer();
 		sb.append("50000, 아이폰, 50,");
 		sb.append("45000, 갤럭시, 100,");
-		sb.append("30000, 노키아, 20");
+		sb.append("30000, 노키아, 20,");
 	}
 
 	// init
@@ -18,11 +21,8 @@ public class ProductService {
 	public ArrayList<ProductDTO> init() {
 
 		String info = sb.toString();
-
-		StringTokenizer st = new StringTokenizer(info, ",");
 		ArrayList<ProductDTO> ar = new ArrayList<ProductDTO>();
-
-		int i = 0;
+		StringTokenizer st = new StringTokenizer(info, ",");
 
 		while (st.hasMoreTokens()) {
 			ProductDTO productDTO = new ProductDTO();
@@ -35,4 +35,37 @@ public class ProductService {
 		return ar;
 	}
 
+	// 기존의 가방에 새상품 추가
+
+	public void addProduct(ArrayList<ProductDTO> ar) {
+		System.out.println("새상품 추가");
+
+		ProductDTO pdto = new ProductDTO();
+		System.out.println("가격 입력");
+		pdto.setPrice(sc.nextInt());
+		System.out.println("제품명 입력");
+		pdto.setName(sc.next());
+		System.out.println("재고 입력");
+		pdto.setStock(sc.nextInt());
+
+		ar.add(pdto);
+
+	}
+
+	// 기존의 가방을 받아 제품 삭제
+	// 일치하는 물건이름 입력받아 삭제
+
+	public int removeProduct(ArrayList<ProductDTO> ar) {
+		System.out.println("삭제할 제품명 입력");
+		String name = sc.next();
+		int result = 0;
+
+		for (int i = 0; i < ar.size(); i++) {
+			if (name.equals(ar.get(i).getName())) {
+				ar.remove(i);
+				result = 0;
+			}
+		}
+		return result;
+	}
 }
